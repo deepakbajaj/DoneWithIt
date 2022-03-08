8924c3810ab004f312d0b39821ba91b00676bf5b
Deepak Bajaj <deepakbajaj85@gmail.com>
2022-03-08 12:01:19 +0000

    Addes landing page

--- App.js
+++ App.js
@@ -1,61 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Image,
-  TouchableHighlight,
-  Button,
-  Alert,
-  Platform,
-} from "react-native";
+import { View } from "react-native";
 
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import React from "react";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  console.log(useDeviceOrientation());
-  // let x = 1;
-  // const handlePress = () => console.log("Text Pressed");
-  return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          height: "30%",
-          width: "50%",
-        }}></View>
-      {/* <Button
-        color={"orange"}
-        title="Click me"
-        onPress={() =>
-          Alert.prompt("Myprompt", "Mypromptmsg", (text) => console.log(text))
-        }
-      /> */}
-      {/* <Text>Hello react native</Text>
-      <Touch onPress={() => console.log("Deepak bajaj")}>
-        <Image
-          source={{
-            width: 200,
-            height: 200,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </Touch>
-      <StatusBar style="auto" /> */}
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // justifyContent: "center",
-    // alignItems: "center",
-    paddingTop: Platform.OS === "android" ? 200 : 0,
-  },
-});
