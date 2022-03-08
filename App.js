6fed33b4e3548e377365a244b2d988cc42355d11
Deepak Bajaj <deepakbajaj85@gmail.com>
2022-03-08 15:49:04 +0000

    padding and margin

--- App.js
+++ App.js
@@ -18,9 +18,21 @@ export default function App() {
           backgroundColor: "dodgerblue",
           width: 100,
           height: 100,
-          borderWidth: 10,
-          borderColor: "royalblue",
-          borderRadius: 50,
+          padding: 20,
+        }}>
+        <View
+          style={{
+            backgroundColor: "gold",
+            width: 50,
+            height: 50,
+          }}></View>
+      </View>
+      <View
+        style={{
+          backgroundColor: "tomato",
+          height: 100,
+          width: 100,
+          margin: 20,
         }}></View>
     </View>
   );
